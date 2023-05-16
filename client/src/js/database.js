2d891e99bf0fb93b3f5cabc43d53537e9bc64066
Matt <mhgarry92@gmail.com>
2023-05-16 17:02:47 -0400

    added logic to store to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
 
+// create connection to db
+const db = await openDB('jate', 1);
+// create a transaction to write data to database and read data from database
+const tx = db.transaction('jate', 'readwrite');
+// open desired object stored in db
+const store = tx.objectStore('jate');
+// store conetnet to db
+const request = store.put( { id: 1, value: content } );
+// if request is sucessful
+const result = await request;
+console.log(result);
+}
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
